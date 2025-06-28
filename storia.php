b271d6af6adae3b07138d921f458860e9dabed10
username5643 <michele.faion@gmail.com>
2025-06-28 19:07:02 +0200

    Delete storia.php

--- storia.php
+++
@@ -1,7 +0,0 @@
-<?php include 'head.html'; ?>
-<div class="container">
-    <h2>Storia di Ragogna</h2>
-    <p>Il territorio di Ragogna è stato abitato fin dall'antichità. Il castello, risalente al Medioevo, fu un importante punto strategico e difensivo. Oggi il paese conserva tradizioni e testimonianze storiche di grande valore.</p>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/2/2e/Ragogna_panorama.jpg" alt="Panorama di Ragogna">
-</div>
-<?php include 'foot.html'; ?>
