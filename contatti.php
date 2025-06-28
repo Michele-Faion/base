cd866dee4b00d6444a45066a9e37a630808ff2b2
username5643 <michele.faion@gmail.com>
2025-06-28 18:50:25 +0200

    Create contatti.php

---
+++ contatti.php
@@ -0,0 +1,16 @@
+<?php include 'head.html'; ?>
+<div class="container">
+    <h2>Contatti</h2>
+    <p>Comune di Ragogna<br>
+    Via Roma 1, 33030 Ragogna (UD)<br>
+    Tel: 0432 123456<br>
+    Email: info@comune.ragogna.ud.it</p>
+    <h3>Orari di apertura</h3>
+    <ul>
+        <li>Lunedì - Venerdì: 8:30 - 13:00</li>
+        <li>Sabato: 9:00 - 12:00</li>
+        <li>Domenica: chiuso</li>
+    </ul>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/3/3e/Ragogna_chiesa.jpg" alt="Chiesa di Ragogna">
+</div>
+<?php include 'foot.html'; ?>
